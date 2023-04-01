15a6eb686102b157f5a95debfe8fd9e633f463bb
chandra-bachtiar <chandrabahtiar25@gmail.com>
2023-04-01 17:09:00 +0700

    Done

---
+++ table.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        clifford: '#da373d',
+                    }
+                }
+            }
+        }
+    </script>
+</head>
+
+<body>
+    <div class="w-full">
+        <div class="grid grid-cols-2">
+            <div>
+                ini table satu
+            </div>
+            <div>
+                ini table dua
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
